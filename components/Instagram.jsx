a6badf512cd601dc52d04bae786aa9bdacc3fb8d
jdonisvitch <jdonisvitch@gmail.com>
2017-02-05 16:07:42 -0500

    attempt to fix instagram

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -11,7 +11,7 @@ class Instagram extends React.Component {
       accessToken: '939099016.1677ed0.e152668a825f4fcf9df8557f4bdf9f99',
       resolution: 'standard_resolution',
       limit: '4',
-      template: '<a href="{{link}}"><img style="height:auto; width:50%;" src="{{image}}\')"></a>'
+      template: '<a href="{{link}}"><img style="height:auto; width:50%;" src="{{image}}"></a>'
     }).run();
 
     return (
